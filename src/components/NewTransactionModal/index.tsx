0d0c5a58a07b22ad54c053e10bf8ea6d06499387
Geraldo <geraldo@easymine.com.br>
2022-01-03 20:09:50 -0300

    api ok

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -5,6 +5,7 @@ import closeImg from "../../assets/close.svg";
 import { Container, TrasactionTypeContainer, RadioBox } from "./styles";
 import { FormEvent, useState } from "react";
 import { type } from "os";
+import { api } from "../../services/api";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -24,12 +25,14 @@ const [type, setType] = useState('deposit');
 function handleCreateNewTransaction(event: FormEvent) {
   event.preventDefault();
 
-  console.log({
+  const data ={
     title,
     value,
     category,
     type
-  })
+  }
+
+  api.post('/transactions', data)
 }
 
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,34 +1,49 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createServer } from "miragejs";
+import { createServer, Model } from "miragejs";
 import { App } from "./App";
-import "./ReactotronConfig";
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+        id: 1,
+        title: 'Freelance',
+        type: 'deposit',
+        category: 'Dev',
+        amount: 6000,
+        createdAt: new Date('2021-02-12 09:00:09'),
+      },
+      {
+        id: 2,
+        title: 'Aluguel',
+        type: 'withdraw',
+        category: 'Casa',
+        amount: 1500,
+        createdAt: new Date('2021-02-12 10:00:09'),
+      },      
+    ],
+    })
+  },
+
   routes() {
     this.urlPrefix = "http://localhost:3000/";
     this.namespace = "api";
 
-    this.get("/transactions", () => ({
-      transactions: [
-        {
-          id: 1,
-          title: "Transation 1",
-          amount: 400,
-          type: "deposit",
-          category: "Food",
-          createdAt: new Date(),
-        },
-        {
-          id: 2,
-          title: "Transation 2",
-          amount: 600,
-          type: "deposit",
-          category: "teste",
-          createdAt: new Date(),
-        },
-      ],
-    }));
+    this.get("/transactions", () => {
+     return this.schema.all('transaction')
+    });
+
+    this.post("/transactions", (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data);
+    })
   },
 });
 
