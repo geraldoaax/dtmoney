b6e7e7a5d06aa11662934624ba49d647a2a37ef1
Geraldo <geraldo@easymine.com.br>
2022-01-04 20:51:01 -0300

    summ

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -11,9 +11,31 @@ import { Container } from "./styles";
 export function Summary() {
 const { transactions } = useContext(TransactionsContext);
 
-console.log(transactions)
+// const totalDeposits = transactions.reduce((acc, transaction) => {
+//     if (transaction.type === 'deposit') {
+//         return acc + transaction.amount;
+//     }
+//     return acc;
+// },0);
 
-    return(
+const summary = transactions.reduce((acc, transaction) => {
+    if (transaction.type === 'deposit') {
+        acc.deposits += transaction.amount;
+        acc.total += transaction.amount;
+    } else {
+        acc.withdraw += transaction.amount;
+        acc.total -= transaction.amount;
+    }
+    return acc;
+}, {
+    deposits: 0,
+    withdraw: 0,
+    total: 0
+})
+
+
+
+    return( 
         <Container>
             {/* <TransactionsContext.Consumer>
                 {(data) => {
@@ -29,21 +51,21 @@ console.log(transactions)
                     <p>Entradas</p>
                     <img src={incomeImg} alt="Entradas"></img>
                 </header>
-                <strong>R$1000,00</strong>
+                <strong>{summary.deposits}</strong>
             </div>
             <div>
                 <header>
                     <p>Saídas</p>
                     <img src={outcomeImg} alt="Entradas"></img>
                 </header>
-                <strong>-R$500,00</strong>
+                <strong>{summary.withdraw}</strong>
             </div>
             <div className="highlight-background">
                 <header>
                     <p>Total</p>
                     <img src={totalImg} alt="Entradas"></img>
                 </header>
-                <strong>R$500,00</strong>
+                <strong>{summary.total}</strong>
             </div>
         </Container>
     )
